8c1b66549a74f3f73cabc5989c4f2dca75ff110d
JvsonKee <jasonk2723@gmail.com>
2024-02-13 13:35:29 -0700

    multithreading

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import concurrent.futures
+import functools
 
 def get_price(listing, listing_id):
     price = listing.find_element(By.XPATH, f'//*[@id="{listing_id}"]/div[2]/hgroup/h3/span').text
@@ -47,34 +49,38 @@ def get_link(listing, listing_id):
     link = listing.find_element(By.XPATH, f'//*[@id="{listing_id}"]/div[1]/a')
     return link.get_attribute('href')
 
+def scrape_listing(listing, community):
+    listing_id = listing.get_attribute("id")
+
+    address = get_address(listing, listing_id)
+    price = get_price(listing, listing_id)
+    r_type = get_type(listing, listing_id)
+    beds = get_beds(listing, listing_id)
+    baths = get_baths(listing, listing_id)
+    area = get_area(listing, listing_id)
+    dom = get_dom(listing, listing_id)
+    link = get_link(listing, listing_id)
+
+    listing_item = {
+        'Address': address,
+        'Community': community,
+        'Price': price,
+        'Type': r_type,
+        'Beds': beds,
+        'Baths': baths,
+        'Sqft': area,
+        'DOM': dom,
+        'Link': link
+    }
+
+    return listing_item
 
 def scrape_listings(listings, community):
     property_list = []
-    for listing in listings:
-        listing_id = listing.get_attribute("id")
-
-        address = get_address(listing, listing_id)
-        price = get_price(listing, listing_id)
-        r_type = get_type(listing, listing_id)
-        beds = get_beds(listing, listing_id)
-        baths = get_baths(listing, listing_id)
-        area = get_area(listing, listing_id)
-        dom = get_dom(listing, listing_id)
-        link = get_link(listing, listing_id)
-
-        listing_item = {
-            'Address': address,
-            'Community': community,
-            'Price': price,
-            'Type': r_type,
-            'Beds': beds,
-            'Baths': baths,
-            'Sqft': area,
-            'DOM': dom,
-            'Link': link
-        }
-
-        property_list.append(listing_item)
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        partial_scrape_listing = functools.partial(scrape_listing, community=community)
+        property_list.extend(executor.map(partial_scrape_listing, listings))
 
     return property_list
 
