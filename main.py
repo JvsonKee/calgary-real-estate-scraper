5045bdde17ae0e4c25a49bc266ca34179ec73c23
JvsonKee <jasonk2723@gmail.com>
2024-02-13 15:47:13 -0700

    code clean up

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-from util import scrape_community
+from util import get_cookies, get_communities, scrape_community
 
-property_list = []
+def main():
+    url = 'https://calgaryhomes.ca/'
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-driver = webdriver.Chrome()
+    cookies = get_cookies(driver)
+    if (cookies):
+        cookies.click()
 
-url = 'https://calgaryhomes.ca/'
-
-driver.get(url)
-
-cookies = driver.find_element(By.CLASS_NAME, 'close-icon')
-
-if (cookies):
-    cookies.click()
-
-community_list = driver.find_element(By.XPATH, '//*[@id="feature-deck"]/div/div')
-communities = community_list.find_elements(By.TAG_NAME, 'article')
-
-for i in range(1,len(communities) + 1):
+    property_list = []
+    communities = get_communities(driver)
     
-    scrape_community(driver, i, property_list)
+    for i in range(1,len(communities) + 1):
+        scrape_community(driver, i, property_list)
+        driver.find_element(By.XPATH, '//*[@id="logo"]').click()
 
-    driver.find_element(By.XPATH, '//*[@id="logo"]').click()
+
+    df = pd.DataFrame(property_list)
+    df.to_csv('out.csv', index=False)
 
 
-df = pd.DataFrame(property_list)
-df.to_csv('out.csv', index=False)
+if __name__ == '__main__':
+    main()
